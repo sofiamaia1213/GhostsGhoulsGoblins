542b2fe95028e0d3586eb61ad8847f2ad3fa3a83
sofiamaia1213 <147747782+sofiamaia1213@users.noreply.github.com>
2025-11-07 11:23:24 -0700

    Initial commit

---
+++ GhostGhoulGoblin.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(patchwork)
+library(ggplot2)
+library(tidymodels)
+library(vroom)
+library(GGally)
+library(rpart)
+library(glmnet)
+library(bonsai)
+library(lightgbm)
+library(agua)
+library(h2o)
+library(kknn)
+library(discrim)
+library(kernlab)
+library(naivebayes)
+library(themis)
+library(beepr)
+
+# Import train and test data
+trainData <- vroom("GitHub/GhostsGhoulsGoblins/train.csv")
+testData <- vroom("GitHub/GhostsGhoulsGoblins/test.csv")
+
+# Recipe
+ggg_recipe <- recipe(type ~ bone_length + rotting_flesh + hair_length + has_soul + color,  
+                     data = trainData) %>%
+
+  
+# Prediction
+predictions <- predict(final_fit, new_data = testData, type = "type")
+  
